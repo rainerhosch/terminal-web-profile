51096ceb7b67cf302e2c292fe933bcd6181a0f5c
rizkyoktan <rizkyoktan@wastukancana.ac.id>
2025-10-31 09:57:27 +0700

    Update background image format to WebP for improved performance and add GitHub link to TerminalHeader for enhanced social media presence.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 import Terminal from '../components/Terminal';
 
+// const sharp = require('sharp');
+
+// sharp('public/images/bg-1.jpeg')
+//   .webp({ quality: 80 })
+//   .toFile('public/images/bg-1.webp')
+//   .then(() => console.log('Konversi selesai!'))
+//   .catch(err => console.error('Gagal konversi:', err));
+
 export default function Home() {
   return (
     // <div className="flex items-center justify-center min-h-screen bg-gray-950 p-4">
     // <div className="flex items-center justify-center min-h-screen p-4 bg-gradient-to-br from-[#0dabff] via-[#00a17f] to-black">
-    <div className="flex items-center justify-center min-h-screen p-4 bg-[url('/images/bg-1.jpeg')] bg-cover bg-center">
+    <div className="flex items-center justify-center min-h-screen p-4 bg-[url('/images/bg-1.webp')] bg-cover bg-center">
       <Terminal />
     </div>
   );
--- src/components/TerminalHeader.js
+++ src/components/TerminalHeader.js
@@ -1,4 +1,4 @@
-import { FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaInstagram, FaLinkedin, FaEnvelope, FaGithub } from 'react-icons/fa';
 
 export default function TerminalHeader() {
     return (
@@ -24,6 +24,17 @@ export default function TerminalHeader() {
                 >
                     <FaInstagram size={18} />
                 </a>
+                <a
+                    href="https://github.com/rainerhosch" // Ganti dengan URL GitHub Anda
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-400 hover:text-white transition-colors"
+                    aria-label="GitHub Profile"
+                >
+                    <FaGithub size={18} />
+                    {/* <svg width="18" height="18" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0C5.372 0 0 5.373 0 12c0 5.303 3.438 9.8 8.207 11.387.6.111.793-.261.793-.577 0-.285-.01-1.04-.016-2.043-3.338.726-4.042-1.616-4.042-1.616-.546-1.387-1.334-1.756-1.334-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.238 1.839 1.238 1.07 1.834 2.806 1.304 3.492.996.108-.776.418-1.304.76-1.604-2.665-.304-5.466-1.332-5.466-5.932 0-1.31.467-2.381 1.236-3.222-.124-.304-.535-1.527.117-3.183 0 0 1.008-.323 3.3 1.23.958-.267 1.984-.4 3.003-.404 1.018.004 2.045.137 3.004.403 2.289-1.553 3.295-1.23 3.295-1.23.653 1.656.242 2.879.12 3.184.77.841 1.235 1.912 1.235 3.222 0 4.61-2.803 5.625-5.473 5.921.43.37.814 1.096.814 2.211 0 1.596-.014 2.884-.014 3.279 0 .319.192.694.801.576C20.565 21.796 24 17.299 24 12c0-6.627-5.373-12-12-12z"/></svg> */}
+                </a>
+                
                 <a
                     href="https://www.linkedin.com/in/rizky-ardiansyah-5ba6b9289/" // Ganti dengan URL LinkedIn Anda
                     target="_blank"
